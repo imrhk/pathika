b3a04605acc9185b702d40fb03d3cb7a11549d8e
Rahul Kumar <rahul.cs.mnnit@gmail.com>
2020-03-14 01:29:20 +0530

    Debug app for development environment

--- app/lib/common/constants.dart
+++ app/lib/common/constants.dart
@@ -1,4 +1,6 @@
-const BASE_URL = "https://cdn.pathika.litedevs.com";
+import 'package:flutter/foundation.dart';
+
+const BASE_URL = kDebugMode ? "http://192.168.1.22" : "https://cdn.pathika.litedevs.com";
 const API_URL = "https://pathika.litedevs.com/api";
 const API_VERSION = 'v1';
 const PRIVACY_POLICY_URL = "https://pathika.litedevs.com/privacy_policy.html";
